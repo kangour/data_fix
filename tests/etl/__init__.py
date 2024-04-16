81f34935b5041d8a9711788d408842daca2ea86b
Miccolo <miccolo@qq.com>
2024-04-16 15:41:08 +0800

    feat(etl/__init__.py): 输出到控制台

--- tests/etl/__init__.py
+++ tests/etl/__init__.py
@@ -1,13 +1,25 @@
 import logging
+import os
 from logging.handlers import RotatingFileHandler
 
 # 先初始化 Django 再配置日志，避免 etl 日志配置被 Django 覆盖。
-test_log_file_path = "ETL.log"
 prefix = "[%(asctime)s] %(filename)s:%(lineno)3d [%(levelname)s] %(message)s"
 # logging.basicConfig(format=prefix, level=logging.INFO)
+formatter = logging.Formatter(prefix)
 
 Logger = logging.getLogger("ETL")
-handler = RotatingFileHandler(test_log_file_path, maxBytes=1024 * 1024 * 100, backupCount=3)
-formatter = logging.Formatter(prefix)
-handler.setFormatter(formatter)
-Logger.addHandler(handler)
+Logger.setLevel(logging.INFO)
+
+# 文件存储路径
+file_dir = os.path.abspath(__file__)
+file_dir = os.path.dirname(file_dir)
+log_file = f"{file_dir}/ETL.log"
+
+rotating = RotatingFileHandler(log_file, maxBytes=1024 * 1024 * 100, backupCount=3)
+
+rotating.setFormatter(formatter)
+Logger.addHandler(rotating)
+
+stream = logging.StreamHandler()  # 往屏幕上输出
+stream.setFormatter(formatter)  # 设置屏幕上显示的格式
+Logger.addHandler(stream)
